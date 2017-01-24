a43eb9128975acaecb7512f66dafbc6a0b3c2fa5
Bjørn Nordlund <bnordlun@cisco.com>
2017-01-24 22:41:32 +0100

    adding devices model

--- lib/cisco_spark/models/devices.rb
+++ lib/cisco_spark/models/devices.rb
@@ -3,7 +3,7 @@ require "cisco_spark/model"
 module CiscoSpark
   class Devices
     include Model
-    resource 'api/v1/organization/1/devices'
+    resource 'api/v1/organization/1eb65fdf-9643-417f-9974-ad72cae0e10f/devices?isHelpDesk=true&checkDisplayName=false&checkOnline=false'
 
     attributes(
       cisUuid: DataCaster::String,
